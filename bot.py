a71d9d49f16d39170541d10f13b39bd94ee8b533
NiceYT <53212206+NiceYT@users.noreply.github.com>
2019-07-30 18:35:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -69,7 +69,7 @@ async def on_message(message):
                 else:
                     Win = "10 робуксов"
                     emb = discord.Embed(title= "10 робуксов", colour = 0xff0404)
-                    emb.add_field(name= f"{message.author} выиграл:" + str(Win), value= "Иконас выдай робуксы {message.author}")
+                    emb.add_field(name= f"{message.author} выиграл:" + str(Win), value= f"Иконас выдай робуксы {message.author}")
                     await channel3.send(embed= emb)
                     await user.add_roles(win1)
             elif case_num <= 201:
